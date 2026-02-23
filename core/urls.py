99b4ad1ac6eee3da6ccd42646be69f4daab898a4
rashaniki <rashaniki@gmail.com>
2026-02-23 02:34:15 +0000

    created render html files

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from core.views import *
 
 urlpatterns = [
-    path ('home', index_view ),
+    path ('', index_view ),
     path ('about' , about_view),
     path('contact', contact_view)
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view (request):
-    return HttpResponse ('home page')
+    return render (request , 'index.html')
 
 def about_view (request):
     return HttpResponse ('about page')
